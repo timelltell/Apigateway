cad7008447fa54814435a3018c0512a4e0cafcbc
timellchen <timellchen@tencent.com>
2021-04-01 21:37:39 +0800

    熔断功能2

--- controller/middle/reverse.go
+++ controller/middle/reverse.go
@@ -1,11 +1,14 @@
 package middle
 
 import (
+	"errors"
+	"github.com/afex/hystrix-go/hystrix"
 	"github.com/gin-gonic/gin"
 	"math/rand"
 	"myproject/Apigateway/config"
 	"net/http"
 	"net/http/httputil"
+	"strconv"
 )
 type BackendMap struct{
 	Domain string
@@ -26,6 +29,30 @@ func Reverse(alias string,conf config.ConfigMap) gin.HandlerFunc{
 		c.Request.URL.Host = randBackend.Domain
 		proxy:=randBackend.Proxy
 		//熔断功能
+		output:=make(chan bool,1)
+		var errChan chan error
+		errChan=hystrix.Go(alias,func()error{
+			defer func(){
+				if r:=recover();r!=nil{
+					errChan<-errors.New("error panic")
+					return
+				}
+			}()
+			proxy.ServeHTTP(c.Writer,c.Request)
+			if c.Writer.Status()>=500{
+				er:=errors.New(strconv.Itoa(c.Writer.Status()))
+				errChan<-er
+				return er
+			}
+			output<-true
+			return nil
+		},nil)
+		select {
+		case <-output:
+			//ok
+		case <-errChan:
+			c.AbortWithStatusJSON(http.StatusInternalServerError,"熔断")
+		}
 		c.Next()
 	}
 }
